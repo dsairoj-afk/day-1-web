405a8a0624fdb0f49f2d7b924ea12bb8520314e7
Sairoj Dangol <dsairoj@gmail.com>
2026-01-10 15:53:24 +0545

    Day 4: Javascript DOM interaction

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function sayHello() {
-  alert("You just wrote your first JavaScript function!");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("greetBtn");
+  const input = document.getElementById("nameInput");
+  const message = document.getElementById("message");
+
+  button.addEventListener("click", function () {
+    const name = input.value;
+
+    if (name === "") {
+      message.textContent = "Please enter your name.";
+    } else {
+      message.textContent = "Hello, " + name + "! Welcome to my website.";
+    }
+  });
+});
+
+
