8585ee1344850c571b92bed11112bde821e4c01c
Sairoj Dangol <dsairoj@gmail.com>
2026-01-11 18:45:44 +0545

    Day 6: Input validation and template literals

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 // TEMP CHANGE
 document.addEventListener("DOMContentLoaded", function () {
-  const input = document.getElementById("nameInput");
+  const nameInput = document.getElementById("nameInput");
+  const ageInput= document.getElementById("ageInput");
   const button = document.getElementById("greetBtn");
   const message = document.getElementById("message");
 
-  message.textContent = "Page loaded. Enter your name and click the button.";
-
   button.addEventListener("click", function () {
-    const name = input.value;
+    const name = nameInput.value.trim();
+    const age = Number(ageInput.value);
 
-    if(name === "") {
-      message.textContent = "You did not enter your name";
-    } else{
-      message.textContent = " Hello "+ name + ", Javascript is working";
+    if(!name) {
+      message.textContent = "Name is required. ";
+      return;
+    } if(!age || age<=0){
+      message.textContent = " Please enter a valid age,";
+      return;
     }
+    message.textContent =
+    age < 18
+    ? `Sorry ${name}, you must be 18 or older.`
+    : `Welcome ${name}, Acess granted.`;
   });
 });
